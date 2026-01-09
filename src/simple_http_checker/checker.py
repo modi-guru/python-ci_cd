a81b2119880fc7341670edc3dbded01801be6b02
Riteek Raj modi <riteekrajmodi@Riteeks-MacBook-Air.local>
2026-01-09 09:59:10 +0530

    new changes

--- src/simple_http_checker/checker.py
+++ src/simple_http_checker/checker.py
@@ -1,11 +1,13 @@
 import logging
-import requests 
+import requests
 
 
 logger = logging.getLogger(__name__)
 
 
-def check_urls(urls: list[str], timeout: int = 5) -> dict[str, str]:
+def check_urls(
+    urls: list[str], timeout: int = 5
+) -> dict[str, str]:
     """
     Check the availability of a list of URLs and their status.
 
@@ -27,8 +29,10 @@ def check_urls(urls: list[str], timeout: int = 5) -> dict[str, str]:
     #         logger.error(f"Error checking {link}: {e}")
     # return results.  ---- these lines are pre populated and should not be use ----
 
-    logger.info(f"Starting URL checks for {len(urls)} URLs with timeout {timeout} seconds.")
-    results = {}
+    logger.info(
+        f"Starting URL checks for {len(urls)} URLs with timeout {timeout} seconds."
+    )
+    results: dict[str, str] = {}
     for url in urls:
         status = "unreachable"
         try:
@@ -38,20 +42,26 @@ def check_urls(urls: list[str], timeout: int = 5) -> dict[str, str]:
             if response.ok:
                 status = f"{response.status_code} OK"
             else:
-                status = f"{response.status_code} {response.reason}"
+                status = (
+                    f"{response.status_code} {response.reason}"
+                )
         except requests.exceptions.Timeout:
             status = "timeout"
             logger.warning(f"Timeout occurred for URL: {url}")
         except requests.exceptions.ConnectionError:
             status = "connection error"
-            logger.warning(f"Connection error occurred for URL: {url}")
+            logger.warning(
+                f"Connection error occurred for URL: {url}"
+            )
         except requests.RequestException as e:
             status = f"request_error:{type(e).__name__}"
-            logger.error(f"An error occurred for URL: {url} - {e}", exc_info=True)
+            logger.error(
+                f"An error occurred for URL: {url} - {e}",
+                exc_info=True,
+            )
 
         results[url] = status
         logger.debug(f"checked:{url:<40}->{status}")
 
     logger.info("Completed URL checks.")
     return results
-
